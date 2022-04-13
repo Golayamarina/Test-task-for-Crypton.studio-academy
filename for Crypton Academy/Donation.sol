5f73eec94ed5541871665393ec1b0907a06655a2
Golayamarina <98704212+Golayamarina@users.noreply.github.com>
2022-04-13 16:48:26 +0300

    Update Donation.sol

--- for Crypton Academy/Donation.sol
+++ for Crypton Academy/Donation.sol
@@ -4,20 +4,19 @@ contract Donation {
 
 address payable public owner;
 address [] public donators;
-uint public donationValue;
-string message;
+mapping(address => uint256) public donations;
+uint256 donationAmount;
 
 constructor () {
 owner = payable(msg.sender);
 }
 
-function currentBalance() public view returns(uint) {
-        return address(this).balance;
+function gatherDonation() public payable {
+donators.push(msg.sender);
 }
 
-function gatherDonation() public payable {
-    //require(msg.value >= 0.01 ether, "Too little money!");
-donators.push(msg.sender);
+function getDonationsAmount() public view returns (uint256) {
+        return donationAmount;
 }
 
 function trasnferToOwner() external {
@@ -29,7 +28,7 @@ function getDonators() public view returns(address [] memory) {
 return donators;
 }
 
-function getDonationValue() public view  returns(uint) {
-return donationValue;
+function getDonations() public view returns(uint256) {
+return donations[msg.sender];
 }
 }
